ab951a154fc79eed665c1f6e5ab5bea2a3d0f18d
Davut Simsek <davutt.simsek@gmail.com>
2025-05-26 13:33:34 +0200

    integrated with dummy backend to show meals

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -5,8 +5,8 @@ export default function MealsGrid({ meals }) {
   return (
     <ul className={classes.meals}>
       {meals.map(meal => (
-        <li key={meals.id}>
-          <MealItem {...meals} />
+        <li key={meal.id}>
+          <MealItem {...meal} />
         </li>
       ))}
     </ul>
