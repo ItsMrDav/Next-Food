eaf18f9624b287e320f7ea2e774f289304e51711
Davut Simsek <davutt.simsek@gmail.com>
2025-07-25 16:52:35 +0200

    created dynamic meal routes

--- app/meals/error.js
+++ app/meals/error.js
@@ -1,5 +1,7 @@
+// In order to catch error even from client side
 'use client';
 
+// Error prop gives information about the details of error
 export default function Error({ error }) {
   return (
     <main className="error">
