5995366ce71bbd47ff4403a009d574c7e61c9b97
Davut Simsek <davutt.simsek@gmail.com>
2025-07-28 19:05:39 +0200

    added dynamic metadatas and checked the production

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,19 @@ import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 import { notFound } from 'next/navigation';
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 // localhost:3000/meals/something (except meals/share cuz it already exist)
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
